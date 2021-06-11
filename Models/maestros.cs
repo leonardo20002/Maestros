7fe09454ce27755b2b3e16ddc30705960c9c8d97
EAbimelec <abimelec70.arenas@gmail.com>
2021-06-11 01:03:00 -0500

    working

--- Models/maestros.cs
+++ Models/maestros.cs
@@ -4,23 +4,11 @@ namespace EscuelaWeb
 {
     public class maestros : DbContext
     {
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Product> Products { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             string path = System.IO.Path.Combine(System.Environment.CurrentDirectory, "maestros.db");
             optionsBuilder.UseSqlite($"Filename = {path}");
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Category>()
-            .Property(category => category.CategoryName)
-            .IsRequired();
-
-            modelBuilder.Entity<Product>()
-            .Property(product => product.Cost)
-            .HasConversion<double>();
-        }
     }
 }
